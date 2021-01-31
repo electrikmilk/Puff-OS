1b45e63d73c9dcee074358e4e66cc0a6045aa8e3
electrikmilk <brandonjordan124@gmail.com>
2021-01-31 17:41:07 -0500

    Started on inline apps, some apps don't need resources loaded in

---
+++ lib/php/inline_app_template.php
@@ -0,0 +1,30 @@
+<?php
+$info = json_decode(file_get_contents("../../apps/$app_name.ap/manifest.json"),true);
+if($info['title'] !== false)$title = $info['name'];
+?>
+<!doctype html>
+<html>
+<head>
+  <title><?php echo $title; ?></title>
+  <script type="text/javascript" src="/lib/apis/app.js"></script>
+  <script>
+  var appTitle = "<?php echo $info['name']; ?>";
+  var appVersion = "<?php echo $info['version']; ?>";
+  </script>
+</head>
+<body style="opacity:0;background:black;pointer-events:none;user-select:none;">
+  <?php
+  if($info['index'])$index = $info['index'];
+  else $index = "index.html";
+  $path = "../../apps/$app_name.ap/$index";
+  if(file_exists($path)) {
+    $ext = pathinfo($path,PATHINFO_EXTENSION);
+    if($ext === "php")include_once $path;
+    else echo file_get_contents($path);
+  } else echo "<script>
+  parent.desktop.log('$app_name.ap DOM not found! Closing...','warn');
+  // apps.close('$app_name');
+  </script>";
+  ?>
+</body>
+</html>
