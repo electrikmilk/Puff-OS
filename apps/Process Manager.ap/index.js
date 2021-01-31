7a9398d4633a238fd327207ca1b591fb961bb09c
electrikmilk <brandonjordan124@gmail.com>
2021-01-30 20:30:30 -0500

    General stuff

--- apps/Process Manager.ap/index.js
+++ apps/Process Manager.ap/index.js
@@ -3,9 +3,11 @@ var cantkill = ["system", "desktop"];
 $(function() {
   setInterval(function() {
     var i = 1;
-    var list = "<table><thead><th></th><th>#</th><th>command</th><th>pid</th><th></th></thead><tbody>";
+    var list = "<table><thead><th></th><th>#</th><th>command</th><th>pid</th><th>type</th><th></th></thead><tbody>";
     main.processes.forEach(function(item) {
       index = i.toString();
+      if (item.app === true) var type = "app";
+      else var type = "system";
       if (index.length === 1) index = "0" + i;
       list += "<tr>";
       if (item.app === true) list += "<td><img src='/apps/" + item.name + ".ap/icon/32.png' width='24'/></td>";
@@ -13,6 +15,7 @@ $(function() {
       list += "<td>" + index + "</td>";
       list += "<td>" + item.name + "</td>";
       list += "<td>" + item.id + "</td>";
+      list += "<td>" + type + "</td>";
       if (!cantkill.includes(item.name)) list += "<td><a href='javascript:;' onclick='main.apps.close(&quot;" + item.id + "&quot;);'>Kill</a></td>";
       else list += "<td></td>";
       list += "</tr>";
@@ -20,7 +23,7 @@ $(function() {
     });
     list += "</tbody></table>";
     if ($(".output").html() !== list) $(".output").html(list);
-  }, 500);
+  }, 300);
 });
 
 function start() {
