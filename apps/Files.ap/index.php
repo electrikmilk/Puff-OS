631e907c1c01e24a00b4083c4824a0642f5f830c
electrikmilk <brandonjordan124@gmail.com>
2021-05-18 14:23:30 -0400

    Fixes, start on Files app

--- apps/Files.ap/index.php
+++ apps/Files.ap/index.php
@@ -1,1 +1,14 @@
-<p>WIP</p>
+<!-- TODO: Use Files class instead -->
+<table>
+<?php
+$path = '../../users/anon'; // default
+if ($handle = opendir($path)) {
+    while (false !== ($entry = readdir($handle))) {
+        if ($entry != "." && $entry != "..") {
+            echo "<tr><td>$entry</td></tr>";
+        }
+    }
+    closedir($handle);
+}
+?>
+</table>
--- lib/php/flush.php
+++ lib/php/flush.php
@@ -1,8 +1,8 @@
 <?php
 /* TODO: each user has their own tmp folder
 and it clears every so often or when they manually clear it */
-$files = Files::getInstance();
-if(!$files->empty("tmp")) {
-  $files->delete("tmp");
-  $files->dir("tmp");
-}
+// $files = Files::getInstance();
+// if(!$files->empty("tmp")) {
+//   $files->delete("tmp");
+//   $files->dir("tmp");
+// }
