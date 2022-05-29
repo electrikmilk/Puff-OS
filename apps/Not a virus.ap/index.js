01f32136bf618999a4801baa4c08c254eaa9bc84
electrikmilk <brandonjordan124@gmail.com>
2022-05-29 17:58:52 -0400

    Update faux mal app

--- apps/Not a virus.ap/index.js
+++ apps/Not a virus.ap/index.js
@@ -1,14 +1,14 @@
-var idontwanttoclose = false;
+let never_close = true;
 
-$(function() {
-  setInterval(function() {
-    main.apps.open("Calculator", true);
-  }, 1000);
-  Window.show();
+$(function () {
+	setInterval(function () {
+		main.apps.open('Calculator', true);
+	}, 2500);
+	Window.show();
 });
 
-function end() {
-  if (idontwanttoclose) {
-    Window.close();
-  }
+function close() {
+	if (!never_close) {
+		Window.close();
+	}
 }
