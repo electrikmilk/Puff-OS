8f730e2e32fc22d8188780bfcfe81e6ca3dccc8b
electrikmilk <brandonjordan124@gmail.com>
2022-05-29 18:04:44 -0400

    Fix 'apps' command in Terminal

--- apps/Terminal.ap/bin/apps.php
+++ apps/Terminal.ap/bin/apps.php
@@ -3,10 +3,10 @@ $escape = false;
 $files = Files::dir("../../apps");
 $apps = array();
 foreach ($files as $app) {
-	array_push($apps, $app['name']);
+	array_push($apps, $app);
 }
 asort($apps);
 foreach ($apps as $app) {
-	$list .= "<div><a href='javascript:;' onclick='main.apps.open(&quot;$app&quot;);'>$app</a></div>";
+	$list .= "<div><a href='javascript:;' onclick='main.apps.open(\"{$app['filename']}\");'>{$app['name']}</a></div>";
 }
 $return .= "<div class='shortlist'>$list</div>";
