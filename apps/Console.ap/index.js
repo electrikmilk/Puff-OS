934797762074c159ca5216c0482ae198edf529f1
electrikmilk <brandonjordan124@gmail.com>
2022-05-29 18:04:32 -0400

    Update Console App

--- apps/Console.ap/index.js
+++ apps/Console.ap/index.js
@@ -1,14 +1,17 @@
-$(function() {
-  setInterval(function() {
-    var list = "";
-    main.log.forEach(function(item) {
-      list += "<li>" + item + "</li>";
-    });
-    if ($(".output ul").html() !== list) $(".output ul").html(list);
-  }, 500);
-  Window.show();
+$(function () {
+	setInterval(function () {
+		let list = '';
+		main.log.forEach(function (item) {
+			list += '<li>' + item + '</li>';
+		});
+		if ($('.output ul').html() !== list) {
+			$('.output ul').html(list);
+		}
+		$('html, body').scrollTop($(document).height());
+	}, 1500);
+	Window.show();
 });
 
 function close() {
-  Window.close();
+	Window.close();
 }
