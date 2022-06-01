96eaea9f59a0d3d55e137f1b1daa48091a8d8852
electrikmilk <brandonjordan124@gmail.com>
2022-05-31 23:43:12 -0400

    Refactor date command again

--- apps/Terminal.ap/bin/date.php
+++ apps/Terminal.ap/bin/date.php
@@ -1,8 +1,27 @@
 <?php
+$command = new Command([
+	'iso' => 'output in iso 8601 format',
+	'epoch' => 'output as unix time',
+	'utc' => 'output as utc',
+	'stamp' => 'output as standard timestamp',
+	'h' => 'output as human readable'
+]);
 if (count($args)) {
-	if ($args[0] === 'utc') {
-		$return = date('c');
+	switch ($args[0]) {
+		case 'iso':
+			$command->output(date('c'));
+			break;
+		case 'epoch':
+			$command->output(time());
+			break;
+		case 'utc':
+			$command->output(gmdate('c'));
+			break;
+		case 'stamp':
+			$command->output(date('Y-m-d H:i:s'));
+			break;
+		case 'h':
+			$command->output(date('l, F j, Y, g:i a'));
+			break;
 	}
-} else {
-	$return = date('Y-m-d H:i:s');
 }
